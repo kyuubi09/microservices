8143bc7d96c05f21166582e06aa4615cdd13af6f
Yubin Shinhmar <200543271@student.georgianc.on.ca>
2025-02-17 15:28:07 -0500

    created versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.87.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  default_tags {
+    tags = {
+        owner = "pafable"
+    }
+  }
+}
